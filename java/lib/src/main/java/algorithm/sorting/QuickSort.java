84828fd4640f87748211a6c5c622018c6c1362a0
Hoshi <hoshimure.47@gmail.com>
2021-08-03 11:04:15 +0900

    :+1: (java) Quick sorting in descending order

--- java/lib/src/main/java/algorithm/sorting/QuickSort.java
+++ java/lib/src/main/java/algorithm/sorting/QuickSort.java
@@ -1,15 +1,16 @@
 package algorithm.sorting;
 
 public class QuickSort extends Sort {
-  private int partition(int[] arr, int low, int high) {
+  private int partition(int[] arr, int low, int high, SortType type) {
     int pivot = arr[high];
 
     int i = low - 1, j = high;
 
     while (true) {
-      while (arr[++i] < pivot)
+      while (super.compare(pivot, arr[++i], type))
         ;
-      while (i < --j && arr[j] > pivot)
+
+      while (i < --j && super.compare(arr[j], pivot, type))
         ;
 
       if (j <= i) {
@@ -22,11 +23,11 @@ public class QuickSort extends Sort {
     return i;
   }
 
-  private int[] quickSort(int[] arr, int low, int high) {
+  private int[] quickSort(int[] arr, int low, int high, SortType type) {
     if (low < high) {
-      int pivot = partition(arr, low, high);
-      quickSort(arr, low, pivot - 1);
-      quickSort(arr, pivot + 1, high);
+      int pivot = partition(arr, low, high, type);
+      quickSort(arr, low, pivot - 1, type);
+      quickSort(arr, pivot + 1, high, type);
     }
 
     return arr;
@@ -35,7 +36,6 @@ public class QuickSort extends Sort {
   @Override
   protected int[] sort(int[] arr, SortType type) {
     int length = arr.length;
-    return quickSort(arr, 0, length - 1);
+    return quickSort(arr, 0, length - 1, type);
   }
-
 }
--- java/lib/src/test/java/algorithm/sorting/QuickSortTest.java
+++ java/lib/src/test/java/algorithm/sorting/QuickSortTest.java
@@ -12,8 +12,11 @@ public class QuickSortTest {
 
     int[] sorted_asc = sorter.sort(arr, SortType.asc);
     int[] expected_asc = { 32, 34, 45, 67, 76, 78 };
-
     assertArrayEquals(expected_asc, sorted_asc);
+
+    int[] sorted_desc = sorter.sort(arr, SortType.desc);
+    int[] expected_desc = { 78, 76, 67, 45, 34, 32 };
+    assertArrayEquals(expected_desc, sorted_desc);
 
     PerfMeasurer perfMeasurer = new PerfMeasurer(sorter);
     System.out.println(perfMeasurer.reportPerf());
