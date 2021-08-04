7e594562702dd7b44d45a45bf6099e402c3294bb
Hoshi <hoshimure.47@gmail.com>
2021-08-04 10:43:55 +0900

    :+1: (py) Merge sorting in descending order.

--- python/sorting/merge_sort.py
+++ python/sorting/merge_sort.py
@@ -1,25 +1,25 @@
-from .sort import Sort
-import sys
+from .sort import Sort, SortType
 
 
 class MergeSort(Sort):
     def __init__(self):
-        # sys.setrecursionlimit(10000)
         self.work_arr = []
 
-    def sort(self, arr: list[int]) -> list[int]:
+    def sort(self, arr: list[int], type: SortType = SortType.asc) -> list[int]:
         length = len(arr)
         self.work_arr = [None] * length
-        return self.merge_sort(arr, 0, length - 1)
+        return self.merge_sort(arr, 0, length - 1, type)
 
-    def merge_sort(self, arr: list[int], low: int, high: int) -> list[int]:
+    def merge_sort(
+        self, arr: list[int], low: int, high: int, type: SortType
+    ) -> list[int]:
         if low >= high:
             return arr
 
         middle: int = int((low + high) / 2)
 
-        self.merge_sort(arr, low, middle)
-        self.merge_sort(arr, middle + 1, high)
+        self.merge_sort(arr, low, middle, type)
+        self.merge_sort(arr, middle + 1, high, type)
 
         for i in range(low, middle + 1):
             self.work_arr[i] = arr[i]
@@ -33,7 +33,7 @@ class MergeSort(Sort):
         j = high
 
         for k in range(low, high + 1):
-            if self.work_arr[i] <= self.work_arr[j]:
+            if self.compare(self.work_arr[j], self.work_arr[i], type):
                 arr[k] = self.work_arr[i]
                 i += 1
             else:
--- python/sorting/merge_sort_test.py
+++ python/sorting/merge_sort_test.py
@@ -1,3 +1,4 @@
+from .sort import SortType
 from .merge_sort import MergeSort
 from .perf_measurer import PerfMeasurer
 
@@ -6,8 +7,13 @@ def test_merge_sort():
     arr = [76, 34, 67, 32, 78, 45]
     sorter = MergeSort()
     sorter.sort(arr)
+
     expected_asc = [32, 34, 45, 67, 76, 78]
     assert arr == expected_asc
+
+    sorter.sort(arr, SortType.desc)
+    expected_desc = [78, 76, 67, 45, 34, 32]
+    assert arr == expected_desc
 
     perf_measurer = PerfMeasurer(sorter)
     print(perf_measurer.reportPerf())
